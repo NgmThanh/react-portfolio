e3ed4dbd328f52c5944f87c9e6a37eadbec8ebd4
ngmthanh <nmthanh99@gmail.com>
2022-07-05 01:16:34 +0200

    homepage updated, summary 1 updated

--- src/components/layouts/HeroMainLayout.js
+++ src/components/layouts/HeroMainLayout.js
@@ -6,15 +6,15 @@ export default function HeroMainLayout() {
     <section id="home" className="main-hero-section">
       <div className="main-hero-background">
         <div className="main-hero-content">
-          <div className="subtitle-small font-gold col-1">Portfolio</div>
+          <div className="spaced-subtitle font-gold col-1">Portfolio</div>
           <div>
-            <h1 className="title-main font-bright">
-              <span className="">Creative</span><br></br>
+            <h1 className="title-main font-bright ml-1">
+              <span className="title-stroke-4">Creative</span><br></br>
               <span className="ml-2">Digital</span><br></br>
               <span className="ml-2">Designer</span>
             </h1>
 
-            <span className="subtitle-medium font-light col-4 ml-3 mt-80">Always in love with code and design, i like to learn new things</span>
+            <span className="subtitle-medium font-light col-4 ml-4 mt-200">Always in love with code and design, i like to learn new things</span>
           </div>
         </div>
       </div>
