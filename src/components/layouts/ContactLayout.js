ad6faa01079d60fcc3434094ad04eed4f8669b59
ngmthanh <nmthanh99@gmail.com>
2022-09-19 23:15:04 +0200

    started implementing contact layout

--- src/components/layouts/ContactLayout.js
+++ src/components/layouts/ContactLayout.js
@@ -1,14 +1,124 @@
 import "../../styles/Contact.scss"
+import React, { Component, useEffect } from 'react';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
 
 export default function ContactLayout() {
 
+  useEffect(() => {
+    // --------------------------------------------------------
+    // Rolling text
+    // https://codepen.io/GreenSock/pen/QWqoKBv?editors=0010
+    gsap.registerPlugin(ScrollTrigger);
+
+    let direction = 1; // 1 = forward, -1 = backward scroll
+
+    const roll1 = roll(".rolling-text", { duration: 10 });
+    const roll2 = roll(".rollingText02", { duration: 10 }, true);
+    const scroll = ScrollTrigger.create({
+      onUpdate(self) {
+        if (self.direction !== direction) {
+          direction *= -1;
+          gsap.to([roll1, roll2], { timeScale: direction, overwrite: true });
+        }
+      }
+    });
+
+    // helper function that clones the targets, places them next to the original, then animates the xPercent in a loop to make it appear to roll across the screen in a seamless loop.
+    function roll(targets, vars, reverse) {
+      vars = vars || {};
+      vars.ease || (vars.ease = "none");
+
+      const tl = gsap.timeline({
+        repeat: -1,
+        onReverseComplete() {
+          this.totalTime(this.rawTime() + this.duration() * 10); // otherwise when the playhead gets back to the beginning, it'd stop. So push the playhead forward 10 iterations (it could be any number)
+        }
+      })
+      const elements = gsap.utils.toArray(targets);
+      const clones = elements.map(el => {
+        let clone = el.cloneNode(true);
+        el.parentNode.appendChild(clone);
+        return clone;
+      });
+      const positionClones = () => elements.forEach((el, i) => gsap.set(clones[i], {
+        position: "absolute", overwrite: false, top: el.offsetTop, left: el.offsetLeft + (reverse ? -el.offsetWidth : el.offsetWidth)
+      }));
+
+      positionClones();
+      elements.forEach((el, i) => tl.to([el, clones[i]], { xPercent: reverse ? 100 : -100, ...vars }, 0));
+
+      window.addEventListener("resize", () => {
+        let time = tl.totalTime(); // record the current time
+        tl.totalTime(0); // rewind and clear out the timeline
+        positionClones(); // reposition
+        tl.totalTime(time); // jump back to the proper time
+      });
+
+      return tl;
+    }
+
+    // --------------------------------------------------------
+    // Follow mouse image
+    // https://armandocanals.com/posts/CSS-transform-rotating-a-3D-object-perspective-based-on-mouse-position.html
+
+    let constrain = 60;
+    let mouseOverContainer = document.querySelector('.contact-section');
+    let layer = document.querySelector('.contact-img');
+
+    function transforms(x, y, el) {
+      let box = el.getBoundingClientRect();
+      let calcX = -(y - box.y - (box.height / 2)) / constrain;
+      let calcY = (x - box.x - (box.width / 2)) / constrain;
+
+      return "perspective(100vw) "
+        + "   rotateX(" + calcX + "deg) "
+        + "   rotateY(" + calcY + "deg) ";
+    };
+
+    function transformElement(el, xyEl) {
+      el.style.transform = transforms.apply(null, xyEl);
+    }
+
+    mouseOverContainer.onmousemove = function (e) {
+      let xy = [e.clientX, e.clientY];
+      let position = xy.concat([layer]);
+
+      window.requestAnimationFrame(function () {
+        transformElement(layer, position);
+      });
+    };
+  }, []);
+
   return (
     <section id="contact" className="contact-section">
-      <div class="wrapper-rolling-text title-200">
-        <div class="rolling-text">
-          <span>Get in touch ·</span> Get in touch · 
+      <div className="wrapper-rolling-text">
+        <div className="rolling-text title-200">
+          <span>Get in touch · Get in touch ·&nbsp;</span>
         </div>
       </div>
+
+      <div className="mid-container">
+        <div className="contact-img-container">
+          <img src="./images/contact-image.jpg" alt="contact" className="contact-img"></img>
+        </div>
+
+        <div className="links">
+          <div className="link-title text">Socials</div>
+          <div className="link text hover-underline-animation"><a href="https://dribbble.com/">Dribbble</a></div>
+          <div className="link text hover-underline-animation"><a href="https://dribbble.com/">Instagram</a></div>
+          <div className="link text hover-underline-animation"><a href="https://dribbble.com/">LinkedIn</a></div>
+        </div>
+
+        <div className="links">
+          <div className="link-title text">Contact</div>
+          <div className="link text hover-underline-animation"><a href="mailto:nmthanh99@gmail.com">nmthanh99@gmail.com</a></div>
+          <div className="link text hover-underline-animation"><a href="tel:+33621617481">+33 6 21 61 74 81</a></div>
+          <div className="link text hover-underline-animation">&#8205;</div>
+        </div>
+      </div>
+
+      <div className="text designed-by">Designed by Minh Thanh.</div>
     </section>
   )
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,54 +4,11 @@ import { gsap } from "gsap";
 
 import ContactLayout from '../layouts/ContactLayout';
 import HeroLayout from '../layouts/HeroLayout';
-import SummaryLayout from '../layouts/SummaryLayout';
-import WorksMainLayout from '../layouts/WorksMainLayout';
 
 export default function Home() {
 
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    let direction = 1; // 1 = forward, -1 = backward scroll
-
-    const roll1 = roll(".rolling-text", { duration: 10 }),
-      roll2 = roll(".rolling-text-2", {duration: 10}, true),
-      scroll = ScrollTrigger.create({
-        onUpdate(self) {
-          if (self.direction !== direction) {
-            direction *= -1;
-            gsap.to([roll1, roll2], { timeScale: direction, overwrite: true });
-          }
-        }
-      });
-
-    // helper function that clones the targets, places them next to the original, then animates the xPercent in a loop to make it appear to roll across the screen in a seamless loop.
-    function roll(targets, vars, reverse) {
-      vars = vars || {};
-      vars.ease || (vars.ease = "none");
-      const tl = gsap.timeline({
-        repeat: -1,
-        onReverseComplete() {
-          this.totalTime(this.rawTime() + this.duration() * 10); // otherwise when the playhead gets back to the beginning, it'd stop. So push the playhead forward 10 iterations (it could be any number)
-        }
-      }),
-        elements = gsap.utils.toArray(targets),
-        clones = elements.map(el => {
-          let clone = el.cloneNode(true);
-          el.parentNode.appendChild(clone);
-          return clone;
-        }),
-        positionClones = () => elements.forEach((el, i) => gsap.set(clones[i], { position: "absolute", overwrite: false, top: el.offsetTop, left: el.offsetLeft + (reverse ? -el.offsetWidth : el.offsetWidth) }));
-      positionClones();
-      elements.forEach((el, i) => tl.to([el, clones[i]], { xPercent: reverse ? 100 : -100, ...vars }, 0));
-      window.addEventListener("resize", () => {
-        let time = tl.totalTime(); // record the current time
-        tl.totalTime(0); // rewind and clear out the timeline
-        positionClones(); // reposition
-        tl.totalTime(time); // jump back to the proper time
-      });
-      return tl;
-    }
+    
   }, []);
 
   return (
