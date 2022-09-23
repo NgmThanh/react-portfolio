92401ba8b0f90e437ac234569870bebd83f80810
ngmthanh <nmthanh99@gmail.com>
2022-09-24 01:10:16 +0200

    fix deployment bug with folder

---
+++ src/components/hooks/useLocoScroll.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import LocomotiveScroll from 'locomotive-scroll';
+import 'locomotive-scroll/src/locomotive-scroll.scss'
+
+export default function useLocoScroll() {
+  useEffect(() => {
+    const scrollEl = document.querySelector('#main-container');
+    const locoScroll = new LocomotiveScroll({
+      el: scrollEl,
+      smooth: true,
+      multiplier: 1,
+      class: 'is-reveal'
+    })
+    
+    // --------------------------------------------------------
+    // Locomotive Anchor Scroll
+    // source : https://github.com/locomotivemtl/locomotive-scroll/issues/175
+    const anchorLinks = document.querySelectorAll('a[href^=\\#]:not([href$=\\#])');
+
+    anchorLinks.forEach((anchorLink) => {
+      let hashval = anchorLink.getAttribute('href');
+      let target = document.querySelector(hashval);
+
+      anchorLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        locoScroll.scrollTo(target);
+      });
+    });
+  }, []);
+}
